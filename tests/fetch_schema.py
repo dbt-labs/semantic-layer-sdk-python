b1a7f35d5ce432070e9c38e9cf76c36f1eda7999
serramatutu <lucas.paes@dbtlabs.com>
2024-06-13 14:55:55 +0200

    chore(schema-fetch): add schema fetch script
    This commit adds a script that will automatically fetch the schema from
    our GraphQL APIs and save it to a local file. This is useful for running
    tests.

---
+++ tests/fetch_schema.py
@@ -0,0 +1,22 @@
+import os
+
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+from graphql import build_client_schema, get_introspection_query, print_schema
+
+HOST = os.environ.get("SL_HOST", "semantic-layer.cloud.getdbt.com")
+
+
+def main():
+    transport = RequestsHTTPTransport(url=f"https://{HOST}/api/graphql", verify=True)
+    client = Client(transport=transport)
+
+    query = gql(get_introspection_query(descriptions=True))
+    result = client.execute(query)
+
+    client_schema = build_client_schema(result)  # type: ignore
+    print(print_schema(client_schema))  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
