56f36dde02e20ddcb0dde78d8122afebbc64cfc6
serramatutu <lucas.paes@dbtlabs.com>
2024-09-06 18:48:19 +0200

    refactor: make error strings better
    This commit makes our error messages better since our errors weren't
    stringifying properly. It also adds a `timeout_ms` parameter to the
    `TimeoutError` so users get a proper feedback of what the current
    timeout is set at.

--- dbtsl/error.py
+++ dbtsl/error.py
@@ -1,9 +1,30 @@
 class SemanticLayerError(RuntimeError):
     """Any errors related to the Semantic Layer inherit from this."""
 
+    def __str__(self) -> str:
+        """RuntimeError doesn't stringify itself by default, so we need to manually add this."""
+        return self.__class__.__name__
+
+    def __repr__(self) -> str:
+        """RuntimeError doesn't stringify itself by default, so we need to manually add this."""
+        return str(self)
+
 
 class TimeoutError(SemanticLayerError):
     """Raise whenever a request times out."""
+
+    def __init__(self, *args, timeout_ms: int, **kwargs) -> None:
+        """Initialize the timeout error.
+
+        Args:
+            timeout_ms: The maximum time limit that got exceeded, in milliseconds
+            *args: any other exception args
+            **kwargs: any other exception kwargs
+        """
+        self.timeout_ms = timeout_ms
+
+    def __str__(self) -> str:  # noqa: D105
+        return f"{self.__class__.__name__}(timeout_ms={self.timeout_ms})"
 
 
 class QueryFailedError(SemanticLayerError):
